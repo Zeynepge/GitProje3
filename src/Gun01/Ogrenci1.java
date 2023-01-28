d4175d12aa8de0682ad8b7115749c949f5e54425
Zeynepge <sgunal571@gmail.com>
2023-01-28 11:34:47 +0100

    2. görev yapildi

--- src/Gun01/Ogrenci1.java
+++ src/Gun01/Ogrenci1.java
@@ -3,5 +3,6 @@ package Gun01;
 public class Ogrenci1 {
     public static void main(String[] args) {
         System.out.println("merhaba");
+        System.out.println("basarili bir sekilde kaydoldu");
     }
 }
